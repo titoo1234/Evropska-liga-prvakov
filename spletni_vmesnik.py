51193e950760f640dd8705fe51062819684f25e3
marinkovicm <48030609+marinkovicm@users.noreply.github.com>
2022-03-10 10:44:47 +0100

    Add files via upload

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,8 +1,12 @@
 import bottle
+import sqlite3 as dbapi
+import model
+
+conn = dbapi.connect('vaja_seminarska_v2.db')
 
 @bottle.get('/')
 def naslovna():
-    return bottle.template("zacetna_stran.html")
+    return bottle.template('zacetna_stran.html', podatki=model.Igralec.najbolsi_strelci_vsa_leta(conn, 10))
 
 
 @bottle.get('/static/<filename:path>')
